f7c8d60ef4fff519318add8310615722dce655b5
Admin <Admin@DESKTOP-U27DABK>
2022-04-01 16:30:50 +0900

    [이찬희] vo 패키지에 Staff 생성, StaffDao 생성 후 selectStaffList 메서드 생성, staffList 출력 메서드를 이용해 리스트 출력과 메뉴탭 구현 후 BootStrap 적용, storeList 메뉴탭 구현 후 BootStrap 적용

---
+++ sakila/src/main/java/dao/StaffDao.java
@@ -0,0 +1,104 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+public class StaffDao {
+	
+	// staffList 출력
+	public List<Map<String, Object>> selectStaffList() {
+		List<Map<String, Object>> list = new ArrayList<>();
+	
+		// DB 접속을 위한 준비
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		String dburl = "jdbc:mariadb://localhost:3306/sakila"; // DB 주소
+		String dbuser = "root"; // DB 아이디
+		String dbpw = "java1234"; // DB 패스워드
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver"); // 드라이버 접속
+			System.out.println("드라이버 로딩 완료"); // 디버깅
+			
+			conn = DriverManager.getConnection(dburl, dbuser, dbpw); // DB 접속
+			System.out.println("conn : " + conn); // 디버깅
+			
+			// staffList 출력 쿼리
+			String sql = "SELECT"
+					+ "	sf.staff_id staffId,"
+					+ "	CONCAT(sf.first_name, ' ', sf.last_name) staffName,"
+					+ "	sf.address_id addressId,"
+					+ "	CONCAT(a.address, IFNULL(a.address2, ''), a.district) staffAddress,"
+					+ "	sf.email email,"
+					+ "	sf.store_id storeId,"
+					+ "	sf.active active,"
+					+ " sf.username username,"
+					+ "	sf.last_update lastUpdate"
+					+ " FROM staff sf INNER JOIN address a"
+					+ " ON sf.address_id = a.address_id;";
+			
+			stmt = conn.prepareStatement(sql); // 쿼리 작성
+			System.out.println("stmt : " + stmt); // 디버깅
+			
+			rs = stmt.executeQuery(); // ResultSet 형식으로 쿼리 저장
+			System.out.println("rs : " + rs); // 디버깅
+			
+			while(rs.next()) {
+				// map 객체를 이용해서 정보들을 저장 후 list에 추가
+				Map<String, Object> map = new HashMap<>();
+				map.put("staffId", rs.getInt("staffId"));
+				map.put("staffName", rs.getString("staffName"));
+				map.put("addressId", rs.getInt("addressId"));
+				map.put("staffAddress", rs.getString("staffAddress"));
+				map.put("email", rs.getString("email"));
+				map.put("storeId", rs.getInt("storeId"));
+				map.put("active", rs.getInt("active"));
+				map.put("username", rs.getString("username"));
+				map.put("lastUpdate", rs.getString("lastUpdate"));
+				list.add(map);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("예외발생");
+		} finally {
+			try {
+				// DB 자원 해지
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return list;
+	}
+	
+	// 단위 테스트
+	public static void main(String[] args) {
+		StaffDao staffDao = new StaffDao(); // 출력 메서드 사용을 위한 객체 생성
+		List<Map<String, Object>> list = staffDao.selectStaffList(); // 출력 메서드의 반환값이 List<Map<String, Object>>형 이므로 객체 생성 후 저장
+		
+		// 출력 부분
+		for(Map m : list) {
+			System.out.print(m.get("staffId") + ", ");
+			System.out.print(m.get("staffName") + ", ");
+			System.out.print(m.get("addressId") + ", ");
+			System.out.print(m.get("staffAddress") + ", ");
+			System.out.print(m.get("email") + ", ");
+			System.out.print(m.get("storeId") + ", ");
+			System.out.print(m.get("active") + ", ");
+			System.out.print(m.get("username") + ", ");
+			System.out.print(m.get("lastUpdate"));
+			System.out.println();
+		}
+	}
+}
--- sakila/src/main/webapp/staffList.jsp
+++ sakila/src/main/webapp/staffList.jsp
@@ -1,30 +1,31 @@
+<%@page import="java.util.*"%>
+<%@page import="dao.StaffDao"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@page import="dao.StoreDao"%>
-<%@ page import="java.util.*" %>
-<%@ page import="java.sql.*" %>
 <%
-	StoreDao storeDao = new StoreDao(); // 출력 메서드 사용을 위한 객체 생성
-	List<Map<String, Object>> list = storeDao.selectStoreList(); // 메서드 실행 후 반환 값 저장
+	StaffDao staffDao = new StaffDao(); // 출력 메서드를 위한 객체 생성
+	List<Map<String, Object>> list = staffDao.selectStaffList(); // 메서드 실행 후 반환값 저장
 %>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Store List</title>
+	<title>Staff List</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 </head>
 <body>
 	<div class="container bg-dark">
 		<jsp:include page="upMenu.jsp"></jsp:include>
-		<h1 class="text-center text-white">Store List</h1>
-		
+		<h1 class="text-center text-white"> Staff List</h1>
 		<table class="table table-bordered text-center table-striped table-dark">
 			<thead>
-				<th>storeId</th>
 				<th>staffId</th>
 				<th>staffName</th>
 				<th>addressId</th>
 				<th>staffAddress</th>
+				<th>email</th>
+				<th>storeId</th>
+				<th>active</th>
+				<th>username</th>
 				<th>lastUpdate</th>
 			</thead>
 			<tbody>
@@ -32,11 +33,14 @@
 					for(Map m : list) {
 				%>
 						<tr>
-							<td><%=m.get("storeId")%></td>
 							<td><%=m.get("staffId")%></td>
 							<td><%=m.get("staffName")%></td>
 							<td><%=m.get("addressId")%></td>
 							<td><%=m.get("staffAddress")%></td>
+							<td><%=m.get("email")%></td>
+							<td><%=m.get("storeId")%></td>
+							<td><%=m.get("active")%></td>
+							<td><%=m.get("username")%></td>
 							<td><%=m.get("lastUpdate")%></td>
 						</tr>
 				<%
